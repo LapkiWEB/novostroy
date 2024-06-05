71a9a9105710fa1b373c13ebbe2cbb5aa49fbefb
Stepan <lapki.web@gmail.com>
2024-06-05 12:40:03 +0800

    add header + adaptive + add section srart-contact + adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,61 @@
 // WOW JS Конфликтует с оверфлоу хайден на бади или секций
+window.onscroll = function showHeader() {
+  let header = document.querySelector('.header');
+  if (window.pageYOffset > 500) {
+    header.classList.add('header__fixed');
+  } else {
+    header.classList.remove('header__fixed');
+  }
+}
+
+$(function () {
+
+
+
+
+
+  $('.menu__btn').on('click', (function () {
+    if (!$('.nav').hasClass('active')) {
+      $('.nav').addClass('active');
+      $('.menu__btn').addClass('active');
+      $('body').addClass('menu-opened');
+      // $('.menu__list-drop').addClass('active');
+      $('.menu__list-drop').on('click', (e) => {
+        e.target.classList.toggle('active');
+      })
+
+    } else {
+      $('.nav').removeClass('active');
+      $('.menu__btn').removeClass('active');
+      $('body').removeClass('menu-opened');
+      // $('.menu__list-drop').removeClass('active');
+      $('.menu__list-drop').on('click', (e) => {
+        e.target.classList.toggle('active');
+      })
+    }
+  }));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
